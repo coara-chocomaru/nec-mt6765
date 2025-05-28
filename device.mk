4dea86b07c557c8e1543474e07e9340338441a3b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-05-28 18:49:06 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service \
-    android.hardware.health@1.0-impl.recovery \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-impl.recovery \
     android.hardware.health@2.0-service
